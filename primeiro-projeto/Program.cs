556b38c8f37adef02f0e449cd42bbd1212505f72
Luiz Miguel Fernandes <luizfgmes0347@gmail.com>
2018-03-13 08:56:35 -0300

    inicio do projeto

---
+++ primeiro-projeto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeiro_projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" sei la poxa");
+        }
+    }
+}
